0cef0122faa5c50d43b4789c57a63713718217ce
Solomon David <solomonjohnson.david@havells.com>
2025-01-20 01:26:43 +0530

    Adding releases for mac and windows

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,6 @@
+# Command to build the application for macOS, windows and linux
 wails build -platform darwin/arm64,linux/amd64,windows/amd64
+
+# Command to zip the application for macOS and windows
+zip Dockernetes_MACOS_arm64.zip build/bin/Dockernetes.app
+zip Dockernetes_MACOS_Windows_AMD64.zip build/bin/Dockernetes-amd64.exe
