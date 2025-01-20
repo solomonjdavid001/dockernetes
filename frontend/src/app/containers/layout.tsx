9ff7c2c79305159a6a84fa917077b79bfd4fc825
SkandaBharadwaj134 <skanda919@gmail.com>
2025-01-20 21:31:20 +0530

    suppress hydration warning and deleted component layout.tsx

--- frontend/src/app/containers/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Sidebar } from "@/components/sidebar/sidebar";
-
-export default function ImagesLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <Sidebar>{children}</Sidebar>;
-}
--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,46 +1,17 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import { ModeToggle } from "../mode-toggle";
 
 export function Header() {
-  const { resolvedTheme, setTheme } = useTheme();
-
-  const toggleTheme = () => {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark");
-  };
-
   return (
     <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4 w-full">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
       </div>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={toggleTheme}
-        className="relative ml-auto"
-      >
-        <Sun
-          className={`h-[1.2rem] w-[1.2rem] transition-all ${
-            resolvedTheme === "dark"
-              ? "rotate-90 scale-0"
-              : "rotate-0 scale-100"
-          }`}
-        />
-        <Moon
-          className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
-            resolvedTheme === "dark"
-              ? "rotate-0 scale-100"
-              : "-rotate-90 scale-0"
-          }`}
-        />
-        <span className="sr-only">Toggle theme</span>
-      </Button>
+      <ModeToggle />
     </header>
   );
 }
